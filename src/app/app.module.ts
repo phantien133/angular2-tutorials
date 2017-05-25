49d8f0f7a4e5d1a7b98798d43c80313533193e7c
Phan Quang Tien <phan.quang.tien@framgia.com>
2017-05-25 11:14:00 +0700

    first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { PeopleComponent } from './people.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PeopleComponent
   ],
   imports: [
     BrowserModule,
